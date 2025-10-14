af465ef34dea55ccdf7777d63d8b5080f448fddd
DN <68803448+777dan@users.noreply.github.com>
2025-10-14 08:14:33 +0300

    Add files via upload

--- resource.h
+++ resource.h
@@ -2,21 +2,34 @@
 // Microsoft Visual C++ generated include file.
 // Used by lb1SproTestVersion.rc
 //
+#define IDCANCEL2                       4
 #define IDR_MENU1                       101
-#define IDC_CURSOR1                     102
 #define IDI_ICON1                       103
+#define IDI_ICON2                       104
+#define IDI_ICON3                       105
+#define IDD_DIALOG2                     109
+#define IDD_DIALOG1                     111
+#define IDDETAILS                       1002
+#define IDC_STATIC2                     1003
 #define ID_CHANGE_ICON                  40001
 #define ID_ABOUTTHEPROGRAM_ABOUT        40002
 #define ID_CHANGE_CURSOR                40003
 #define ID_CHANGE_ICON40004             40004
+#define ID_40005                        40005
+#define ID_40006                        40006
+#define ID_CHANGE_ICON1                 40007
+#define ID_CHANGE_ICON2                 40008
+#define ID_CHANGE_ICON3                 40009
+#define ID_ABOUT_PROGRAM                40011
+#define IDC_STATIC                      -1
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        104
-#define _APS_NEXT_COMMAND_VALUE         40005
-#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_RESOURCE_VALUE        113
+#define _APS_NEXT_COMMAND_VALUE         40012
+#define _APS_NEXT_CONTROL_VALUE         1004
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
--- source.cpp
+++ source.cpp
@@ -1,30 +1,34 @@
-#include <windows.h> // підключення бібліотеки з функціями API
+п»ї#include <windows.h> // РїС–РґРєР»СЋС‡РµРЅРЅСЏ Р±С–Р±Р»С–РѕС‚РµРєРё Р· С„СѓРЅРєС†С–СЏРјРё API
 #include "resource.h"
-// Глобальні змінні:
-HINSTANCE hInst; 	//Дескриптор програми	
+#include <shellapi.h>
+
+// Р“Р»РѕР±Р°Р»СЊРЅС– Р·РјС–РЅРЅС–:
+HINSTANCE hInst; 	//Р”РµСЃРєСЂРёРїС‚РѕСЂ РїСЂРѕРіСЂР°РјРё	
 LPCTSTR szWindowClass = "QWERTY";
 LPCTSTR szTitle = "Ivaknenko Diana";
+NOTIFYICONDATA tray;
+HWND g_hMainWnd;
 
-// Попередній опис функцій
+// РџРѕРїРµСЂРµРґРЅС–Р№ РѕРїРёСЃ С„СѓРЅРєС†С–Р№
 
 ATOM MyRegisterClass(HINSTANCE hInstance);
 BOOL InitInstance(HINSTANCE, int);
 LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
-// Основна програма 
+// РћСЃРЅРѕРІРЅР° РїСЂРѕРіСЂР°РјР° 
 int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine,
 	int nCmdShow)
 {
 	MSG msg;
 
-	// Реєстрація класу вікна 
+	// Р РµС”СЃС‚СЂР°С†С–СЏ РєР»Р°СЃСѓ РІС–РєРЅР° 
 	MyRegisterClass(hInstance);
 
-	// Створення вікна програми
+	// РЎС‚РІРѕСЂРµРЅРЅСЏ РІС–РєРЅР° РїСЂРѕРіСЂР°РјРё
 	if (!InitInstance(hInstance, nCmdShow))
 	{
 		return FALSE;
 	}
-	// Цикл обробки повідомлень
+	// Р¦РёРєР» РѕР±СЂРѕР±РєРё РїРѕРІС–РґРѕРјР»РµРЅСЊ
 	while (GetMessage(&msg, NULL, 0, 0))
 	{
 		TranslateMessage(&msg);
@@ -37,51 +41,104 @@ ATOM MyRegisterClass(HINSTANCE hInstance)
 {
 	WNDCLASSEX wcex;
 	wcex.cbSize = sizeof(WNDCLASSEX);
-	wcex.style = CS_VREDRAW | CS_HREDRAW | CS_DBLCLKS;  		//стиль вікна
-	wcex.lpfnWndProc = (WNDPROC)WndProc; 		//віконна процедура
+	wcex.style = CS_VREDRAW | CS_HREDRAW | CS_DBLCLKS;  		//СЃС‚РёР»СЊ РІС–РєРЅР°
+	wcex.lpfnWndProc = (WNDPROC)WndProc; 		//РІС–РєРѕРЅРЅР° РїСЂРѕС†РµРґСѓСЂР°
 	wcex.cbClsExtra = 0;
 	wcex.cbWndExtra = 0;
-	wcex.hInstance = hInstance; 			//дескриптор програми
-	wcex.hIcon = LoadIcon(NULL, IDI_APPLICATION); 		//визначення іконки
-	wcex.hCursor = LoadCursor(NULL, IDC_ARROW); 	//визначення курсору
-	wcex.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH); //установка фону
-	wcex.lpszMenuName = MAKEINTRESOURCE(IDR_MENU1);				//визначення меню
-	wcex.lpszClassName = szWindowClass; 		//ім’я класу
+	wcex.hInstance = hInstance; 			//РґРµСЃРєСЂРёРїС‚РѕСЂ РїСЂРѕРіСЂР°РјРё
+	wcex.hIcon = LoadIcon(hInstance, IDI_APPLICATION); 		//РІРёР·РЅР°С‡РµРЅРЅСЏ С–РєРѕРЅРєРё
+	wcex.hIconSm = LoadIcon(hInstance, IDI_APPLICATION);
+	wcex.hCursor = LoadCursor(NULL, IDC_ARROW); 	//РІРёР·РЅР°С‡РµРЅРЅСЏ РєСѓСЂСЃРѕСЂСѓ
+	wcex.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH); //СѓСЃС‚Р°РЅРѕРІРєР° С„РѕРЅСѓ
+	wcex.lpszMenuName = MAKEINTRESOURCE(IDR_MENU1);				//РІРёР·РЅР°С‡РµРЅРЅСЏ РјРµРЅСЋ
+	wcex.lpszClassName = szWindowClass; 		//С–РјвЂ™СЏ РєР»Р°СЃСѓ
 	wcex.hIconSm = NULL;
-	return RegisterClassEx(&wcex); 			//реєстрація класу вікна
+	return RegisterClassEx(&wcex); 			//СЂРµС”СЃС‚СЂР°С†С–СЏ РєР»Р°СЃСѓ РІС–РєРЅР°
 }
 
 // FUNCTION: InitInstance (HANDLE, int)
-// Створює вікно програми і зберігає дескриптор програми в змінній hInst
+// РЎС‚РІРѕСЂСЋС” РІС–РєРЅРѕ РїСЂРѕРіСЂР°РјРё С– Р·Р±РµСЂС–РіР°С” РґРµСЃРєСЂРёРїС‚РѕСЂ РїСЂРѕРіСЂР°РјРё РІ Р·РјС–РЅРЅС–Р№ hInst
 
 BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
 {
 	HWND hWnd;
-	hInst = hInstance; //зберігає дескриптор додатка в змінній hInst
-	hWnd = CreateWindow(szWindowClass, 	// ім’я класу вікна
-		szTitle, 				// назва програми
-		WS_CAPTION | WS_BORDER | WS_HSCROLL,			// стиль вікна
-		10, 			// положення по Х	
-		30,			// положення по Y	
-		400, 			// розмір по Х
-		300, 			// розмір по Y
-		NULL, 					// дескриптор батьківського вікна	
-		NULL, 					// дескриптор меню вікна
-		hInstance, 				// дескриптор програми
-		NULL); 				// параметри створення.
+	hInst = hInstance; //Р·Р±РµСЂС–РіР°С” РґРµСЃРєСЂРёРїС‚РѕСЂ РґРѕРґР°С‚РєР° РІ Р·РјС–РЅРЅС–Р№ hInst
+	hWnd = CreateWindow(szWindowClass, 	// С–РјвЂ™СЏ РєР»Р°СЃСѓ РІС–РєРЅР°
+		szTitle, 				// РЅР°Р·РІР° РїСЂРѕРіСЂР°РјРё
+		WS_OVERLAPPED | WS_CAPTION | WS_SYSMENU | WS_MINIMIZEBOX, // СЃС‚РёР»СЊ РІС–РєРЅР°
+		10, 			// РїРѕР»РѕР¶РµРЅРЅСЏ РїРѕ РҐ	
+		30,			// РїРѕР»РѕР¶РµРЅРЅСЏ РїРѕ Y	
+		400, 			// СЂРѕР·РјС–СЂ РїРѕ РҐ
+		300, 			// СЂРѕР·РјС–СЂ РїРѕ Y
+		NULL, 					// РґРµСЃРєСЂРёРїС‚РѕСЂ Р±Р°С‚СЊРєС–РІСЃСЊРєРѕРіРѕ РІС–РєРЅР°	
+		NULL, 					// РґРµСЃРєСЂРёРїС‚РѕСЂ РјРµРЅСЋ РІС–РєРЅР°
+		hInstance, 				// РґРµСЃРєСЂРёРїС‚РѕСЂ РїСЂРѕРіСЂР°РјРё
+		NULL); 				// РїР°СЂР°РјРµС‚СЂРё СЃС‚РІРѕСЂРµРЅРЅСЏ.
+	g_hMainWnd = hWnd;
 
-	if (!hWnd) 	//Якщо вікно не творилось, функція повертає FALSE
+	if (!hWnd) 	//РЇРєС‰Рѕ РІС–РєРЅРѕ РЅРµ С‚РІРѕСЂРёР»РѕСЃСЊ, С„СѓРЅРєС†С–СЏ РїРѕРІРµСЂС‚Р°С” FALSE
 	{
 		return FALSE;
 	}
-	ShowWindow(hWnd, nCmdShow); 		//Показати вікно
-	UpdateWindow(hWnd); 				//Оновити вікно
+	ShowWindow(hWnd, nCmdShow); 		//РџРѕРєР°Р·Р°С‚Рё РІС–РєРЅРѕ
+	UpdateWindow(hWnd); 				//РћРЅРѕРІРёС‚Рё РІС–РєРЅРѕ
 	return TRUE;
 }
 
-// FUNCTION: WndProc (HWND, unsigned, WORD, LONG)
-// Віконна процедура. Приймає і обробляє всі повідомлення, що приходять в додаток
+INT_PTR CALLBACK DialogProc2(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	switch (message)
+	{
+	case WM_COMMAND:
+		switch (LOWORD(wParam))
+		{
+		case WM_INITDIALOG:  //С–РЅС–С†С–Р°Р»С–Р·Р°С†С–СЏ С„СѓРЅРєС†С–РѕРЅР°Р»Сѓ РєРµСЂСѓРІР°РЅРЅСЏ РґС–Р°Р»РѕРіРѕРІРёРј РІС–РєРЅРѕРј
+			return TRUE;
+		case IDOK:
+			EndDialog(hDlg, IDOK);
+			SendMessage(g_hMainWnd, WM_CLOSE, 0, 0);
+			break;
+		case IDCANCEL2:
+			EndDialog(hDlg, IDCANCEL2);
+			break;
+		}
+		break;
+	case WM_CLOSE: 
+		EndDialog(hDlg, 0);
+		return TRUE;
+	}
+	return FALSE;
+}
 
+// FUNCTION: WndProc (HWND, unsigned, WORD, LONG)
+// Р’С–РєРѕРЅРЅР° РїСЂРѕС†РµРґСѓСЂР°. РџСЂРёР№РјР°С” С– РѕР±СЂРѕР±Р»СЏС” РІСЃС– РїРѕРІС–РґРѕРјР»РµРЅРЅСЏ, С‰Рѕ РїСЂРёС…РѕРґСЏС‚СЊ РІ РґРѕРґР°С‚РѕРє
+
+INT_PTR CALLBACK DialogProc(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	switch (message)
+	{
+	case WM_COMMAND:
+		switch (LOWORD(wParam))
+		{
+		case WM_INITDIALOG:  //С–РЅС–С†С–Р°Р»С–Р·Р°С†С–СЏ С„СѓРЅРєС†С–РѕРЅР°Р»Сѓ РєРµСЂСѓРІР°РЅРЅСЏ РґС–Р°Р»РѕРіРѕРІРёРј РІС–РєРЅРѕРј
+			return TRUE;
+		case IDOK:
+			EndDialog(hDlg, IDOK);
+			break;
+		case IDCLOSE:
+			DialogBox(hInst, MAKEINTRESOURCE(IDD_DIALOG1), hDlg, DialogProc2);
+			break;
+		case IDDETAILS:
+			SetDlgItemText(hDlg, IDC_STATIC2, "РљР†РЈРљР†-24-1");
+			break;
+		}
+		break;
+	case WM_CLOSE:
+		EndDialog(hDlg, 0);
+		return TRUE;
+	}
+	return FALSE;
+}
 
 LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
@@ -91,35 +148,68 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 
 	switch (message)
 	{
-	case WM_CREATE: 				//Повідомлення приходить при створенні вікна
+	case WM_CREATE: 		
+	{
+		tray.cbSize = sizeof(NOTIFYICONDATA);
+		tray.hWnd = hWnd;
+		tray.uID = 1;
+		tray.uFlags = NIF_ICON;
+		tray.uCallbackMessage = NULL;
+		tray.hIcon = LoadIcon(hInst, MAKEINTRESOURCE(IDI_APPLICATION));
+
+		Shell_NotifyIcon(NIM_ADD, &tray);
 		break;
+	}
+
 	case WM_COMMAND:
+	{
+		HICON hIcon = NULL;
+
 		switch (LOWORD(wParam))
 		{
-		case ID_CHANGE_CURSOR:
-			SetClassLongPtr(hWnd, GCLP_HCURSOR, (LONG_PTR)LoadCursor(hInst, MAKEINTRESOURCE(IDC_CURSOR1)));
-		
-		break;
-		case ID_CHANGE_ICON40004:
-			SetClassLongPtr(hWnd, GCLP_HICON, (LONG_PTR)LoadIcon(hInst, MAKEINTRESOURCE(IDI_ICON1)));
-		break;
+		case ID_CHANGE_ICON1:
+			hIcon = LoadIcon(hInst, MAKEINTRESOURCE(IDI_ICON1));
+			break;
+		case ID_CHANGE_ICON2:
+			hIcon = LoadIcon(hInst, MAKEINTRESOURCE(IDI_ICON2));
+			break;
+		case ID_CHANGE_ICON3:
+			hIcon = LoadIcon(hInst, MAKEINTRESOURCE(IDI_ICON3));
+			break;
+		case ID_ABOUT_PROGRAM:
+			DialogBox(hInst, MAKEINTRESOURCE(IDD_DIALOG2), hWnd, DialogProc);
+			break;
+		default:
+			return DefWindowProc(hWnd, message, wParam, lParam);
 		}
+
+		if (hIcon)
+		{
+			SendMessage(hWnd, WM_SETICON, ICON_BIG, (LPARAM)hIcon);
+			SendMessage(hWnd, WM_SETICON, ICON_SMALL, (LPARAM)hIcon);
+
+			tray.hIcon = hIcon;
+			Shell_NotifyIcon(NIM_MODIFY, &tray);
+		}
+	}
+	break;
+
 	case WM_LBUTTONDBLCLK:
-		//Подвійне натискання лівої кнопки миші 
+		//РџРѕРґРІС–Р№РЅРµ РЅР°С‚РёСЃРєР°РЅРЅСЏ Р»С–РІРѕС— РєРЅРѕРїРєРё РјРёС€С– 
 		MessageBox(hWnd, "TEXT", "CAPTION", MB_OK);
 		break;
-	case WM_PAINT: 				//Перемалювати вікно
-		hdc = BeginPaint(hWnd, &ps); 	//Почати графічний вивід	
-		GetClientRect(hWnd, &rt); 		//Область вікна для малювання
-		DrawText(hdc, "Привіт, світ!", -1, &rt, DT_SINGLELINE | DT_CENTER | DT_VCENTER);
-		EndPaint(hWnd, &ps); 		//Закінчити графічний вивід	
+	case WM_PAINT: 				//РџРµСЂРµРјР°Р»СЋРІР°С‚Рё РІС–РєРЅРѕ
+		hdc = BeginPaint(hWnd, &ps); 	//РџРѕС‡Р°С‚Рё РіСЂР°С„С–С‡РЅРёР№ РІРёРІС–Рґ	
+		GetClientRect(hWnd, &rt); 		//РћР±Р»Р°СЃС‚СЊ РІС–РєРЅР° РґР»СЏ РјР°Р»СЋРІР°РЅРЅСЏ
+		DrawText(hdc, "РџСЂРёРІС–С‚, СЃРІС–С‚!", -1, &rt, DT_SINGLELINE | DT_CENTER | DT_VCENTER);
+		EndPaint(hWnd, &ps); 		//Р—Р°РєС–РЅС‡РёС‚Рё РіСЂР°С„С–С‡РЅРёР№ РІРёРІС–Рґ	
 		break;
 
-	case WM_DESTROY: 				//Завершення роботи
+	case WM_DESTROY: 				//Р—Р°РІРµСЂС€РµРЅРЅСЏ СЂРѕР±РѕС‚Рё
 		PostQuitMessage(0);
 		break;
 	default:
-		//Обробка повідомлень, які не оброблені користувачем
+		//РћР±СЂРѕР±РєР° РїРѕРІС–РґРѕРјР»РµРЅСЊ, СЏРєС– РЅРµ РѕР±СЂРѕР±Р»РµРЅС– РєРѕСЂРёСЃС‚СѓРІР°С‡РµРј
 		return DefWindowProc(hWnd, message, wParam, lParam);
 	}
 	return 0;
