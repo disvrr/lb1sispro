c5eae544ea234b49c57f279d7564cd76647d67e8
disvrr <dirostiv@gmail.com>
2025-10-31 09:29:57 +0200

    commited

--- resource.h
+++ resource.h
@@ -9,8 +9,12 @@
 #define IDI_ICON3                       105
 #define IDD_DIALOG2                     109
 #define IDD_DIALOG1                     111
+#define IDD_DIALOG3                     113
 #define IDDETAILS                       1002
 #define IDC_STATIC2                     1003
+#define IDC_CHECK1                      1006
+#define IDC_EDIT1                       1007
+#define IDC_BUTTON1                     1008
 #define ID_CHANGE_ICON                  40001
 #define ID_ABOUTTHEPROGRAM_ABOUT        40002
 #define ID_CHANGE_CURSOR                40003
@@ -21,15 +25,15 @@
 #define ID_CHANGE_ICON2                 40008
 #define ID_CHANGE_ICON3                 40009
 #define ID_ABOUT_PROGRAM                40011
-#define IDC_STATIC                      -1
+#define ID_40012                        40012
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        113
-#define _APS_NEXT_COMMAND_VALUE         40012
-#define _APS_NEXT_CONTROL_VALUE         1004
+#define _APS_NEXT_RESOURCE_VALUE        115
+#define _APS_NEXT_COMMAND_VALUE         40013
+#define _APS_NEXT_CONTROL_VALUE         1009
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
